e8ffdafe01a12d48cf35ad676b042fc116298813
Dmytro Akulshyn <petrysho2288@gmail.com>
2026-06-07 17:41:59 -0600

    Lesson5

---
+++ main.js
@@ -0,0 +1,30 @@
+let menu = document.querySelector(".fixed-menu");
+
+menu.addEventListener('click', function(event){
+   let clickTarget = event.target;
+   let activeBtn = document.querySelector('.active');
+   
+   if (clickTarget.classList.contains('nav-link')){
+       clickTarget.classList.add('active');
+       activeBtn.classList.remove('active');
+   }
+});
+
+let classLink = '.main-link';
+
+
+window.onscroll = function() {
+    let h = document.documentElement.clientHeight;
+
+    if (window.scrollY >= h*4) {
+        classLink = '.comments-link';
+    }
+    else if (window.scrollY >= h*3) {
+        classLink = '.works-link';
+    }
+    else if (window.scrollY >= h*3) {
+        classLink = '.works-link';
+    }
+    else if (window.scrollY >= h*3) {
+        classLink = '.works-link';
+    }
